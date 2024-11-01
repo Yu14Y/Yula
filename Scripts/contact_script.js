a79d03eed687a6e96d9c4f58dd7651f5faedd36a
Yu14 Ywen <yu14@Yu14.Ywen>
2024-11-01 20:15:23 +0630

    contact us

---
+++ Scripts/contact_script.js
@@ -0,0 +1,35 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+    const subject = document.getElementById('subject').value;
+    const discordUid = document.getElementById('discordUid').value;
+    const text = document.getElementById('text').value;
+
+    const webhookURL = ""
+
+    const message = {
+        content: `**Email:** ${email}\n**Name:** ${name}\n**Discord UID:** ${discordUid}\n**Subject:** ${subject}\n**Message:** ${text}`
+    };
+
+    fetch(webhookURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message)
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Message sent successfully!');
+            document.getElementById('contactForm').reset(); // Reset the form
+        } else {
+            alert('Error sending message.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error sending message.');
+    });
+});
