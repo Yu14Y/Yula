06eaaf83dec5f0672e379b9589cb8a300167f021
Yu14 <180413739+Yu14Y@users.noreply.github.com>
2025-01-12 20:43:11 +0630

    .

--- Scripts/contact_script.js
+++ Scripts/contact_script.js
@@ -8,13 +8,13 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     const text = document.getElementById('text').value;
     // Note, I plan to add my webhook url in an env variable in the future, For now if you spam my webhook, well theres nothing i can do about it.//
     // Also i made a bot that will auto delete any message containing @everyone and any message that is not formatted the way it is, if user sends 2 or more of the same message, bot will auto delete. I don't plan to open source this bot anytime soon as people might find ways to abuse it. //
-    const webhookURL = "https://canary.discord.com/api/webhooks/1326020075391811644/47SzdcZzU6A4i_HQLGZRjl4N73KvoazeyIeS1jjaunchAkoef3OXs94MlNssFD3_KkOE"
+    const a = "https://canary.discord.com/api/webhooks/1328003587388870776/n5r8cC9TD1Wfn1uibtBkjeo9JAq50lvCxA7brSt3HmRxvnYoMRMWUkgR4r1HoJPJbPkf"
 
     const message = {
         content: `**Email:** ${email}\n**Name:** ${name}\n**Discord UID:** ${discordUid}\n**Subject:** ${subject}\n**Message:** ${text} \n ========`
     };
 
-    fetch(webhookURL, {
+    fetch(a, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
